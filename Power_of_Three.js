9353e42afe3fe35e771af88bb22446d6bfe48a40
JulianaYo <julianatsvetkova@mail.ru>
2019-11-09 20:45:26 +0200

    submission info has been added

--- Power_of_Three.js
+++ Power_of_Three.js
@@ -1,4 +1,8 @@
 /** https://leetcode.com/problems/power-of-three/*/
+// 21038 / 21038 test cases passed.
+// 9/11/2019 submission date
+// Runtime: 224 ms, faster than 41.52% of JavaScript online submissions for Power of Three.
+// Memory Usage: 47.9 MB, less than 60.00% of JavaScript online submissions for Power of Three.
 
 var isPowerOfThree = function(n) {
     if (n === 0) {
