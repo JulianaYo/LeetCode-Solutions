cf2e806bc5fed8027982972cba52ab56ea89b7bb
JulianaYo <julianatsvetkova@mail.ru>
2019-11-10 11:33:25 +0200

    trim method has been optimised

--- Length_of_Last_Word.js
+++ Length_of_Last_Word.js
@@ -1,21 +1,19 @@
 // https://leetcode.com/problems/length-of-last-word/
 // 30/10/2019
 // 59 / 59 test cases passed.
-// Runtime: 52 ms, faster than 76.83% of JavaScript online submissions for Length of Last Word.
-// Memory Usage: 33.8 MB, less than 80.77% of JavaScript online submissions for Length of Last Word.
+// Runtime: 44 ms, faster than 97.82% of JavaScript online submissions for Length of Last Word.
+// Memory Usage: 33.7 MB, less than 92.31% of JavaScript online submissions for Length of Last Word.
 
 /**
  * @param {string} s
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    String.prototype.rtrim = function () {
-         return this.replace(/((\s*\S+)*)\s*/, "$1");
+    String.prototype.rltrim = function () {
+         return this.replace(/((\s*\S+)*)\s*((\S+\s*)*)/, "$1");
      }
-     String.prototype.ltrim = function () {
-         return this.replace(/\s*((\S+\s*)*)/, "$1");
-     }
-     var arr = s.rtrim().ltrim().split(' ')
+
+     var arr = s.rltrim().split(' ')
      
      if (arr.length > 0) {
          return arr[arr.length - 1].length
